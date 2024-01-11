f646cd15252bcb7c46ed7a5f27742182c87be5ee
sergiomartins8 <sergio.martins@onfido.com>
2024-01-11 14:22:51 +0000

    cache build

--- app/src/androidTest/java/com/onfido/evergreen/WebViewIntegrationTest.kt
+++ app/src/androidTest/java/com/onfido/evergreen/WebViewIntegrationTest.kt
@@ -6,11 +6,13 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class OpenAppTest {
 
     @get:Rule
