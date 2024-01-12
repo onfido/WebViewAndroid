0b7b3a605acf55e0fad88858636ecc3a1a530df6
sergiomartins8 <sergio.martins@onfido.com>
2024-01-12 14:56:24 +0000

    debug run

--- app/src/androidTest/java/com/onfido/evergreen/WebViewIntegrationTest.kt
+++ app/src/androidTest/java/com/onfido/evergreen/WebViewIntegrationTest.kt
@@ -1,27 +1,25 @@
 package com.onfido.evergreen
 
-import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class OpenAppTest {
+class WebViewIntegrationTest {
 
     @get:Rule
-    val activityScenarioRule = ActivityTestRule(MainActivity::class.java)
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
 
     @Test
     fun openApp() {
-        activityScenarioRule.launchActivity(Intent())
         onView(withId(R.id.webview)).check(matches(isDisplayed()))
     }
 }
