0e020ca54c6b6c58501da5a06ab1e268da5379c6
sergiomartins8 <sergio.martins@onfido.com>
2024-01-13 08:57:16 +0000

    grant camera permissions

--- app/src/androidTest/java/com/onfido/evergreen/WebViewIntegrationTest.kt
+++ app/src/androidTest/java/com/onfido/evergreen/WebViewIntegrationTest.kt
@@ -1,5 +1,6 @@
 package com.onfido.evergreen
 
+import android.Manifest
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -7,13 +8,22 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class WebViewIntegrationTest {
+    @get:Rule
+    var permissionCamera: GrantPermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.CAMERA)
+
+    @get:Rule
+    var permissionAudio: GrantPermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.RECORD_AUDIO)
 
     @get:Rule
     var activityScenarioRule = activityScenarioRule<MainActivity>()
